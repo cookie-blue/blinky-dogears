de1e2b2cd40a7589144f331f0108ff7d6ed7d6cf
Cookie Blue <mail@cookie-blue.de>
2026-05-07 22:33:36 +0200

    fix brightness override

--- src/led.cpp
+++ src/led.cpp
@@ -3,12 +3,12 @@
 void LED::write(uint8_t ledPin, uint8_t brightness)
 {
     uint8_t pattern = PatternManager::getCurrentPattern();
-    float patternBrightness = (patterns[pattern].brightness / 255);
+    float patternBrightness = (patterns[pattern].brightness / 255) * brightness;
 
     if (PatternManager::brightnessOverride > 0) {
-        patternBrightness = (PatternManager::brightnessOverride / 255);
+        patternBrightness = (PatternManager::brightnessOverride / 255) * brightness;
     }
 
-    DUTY_TYPE gammaValue = PGM_READ_DUTY(&gammaMap[brightness]);
+    DUTY_TYPE gammaValue = PGM_READ_DUTY(&gammaMap[(uint8_t)brightness]);
     ledcWrite(ledPin, gammaValue);
 }
