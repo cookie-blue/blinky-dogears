28a7ba09ec19c43884aac2a27a84e8591d0a6c9e
Cookie Blue <mail@cookie-blue.de>
2026-05-07 22:46:02 +0200

    fix division

--- src/led.cpp
+++ src/led.cpp
@@ -3,10 +3,10 @@
 void LED::write(uint8_t ledPin, uint8_t brightness)
 {
     uint8_t pattern = PatternManager::getCurrentPattern();
-    float patternBrightness = (patterns[pattern].brightness / 255) * brightness;
+    float patternBrightness = brightness * patterns[pattern].brightness / 255;
 
     if (PatternManager::brightnessOverride > 0) {
-        patternBrightness = (PatternManager::brightnessOverride / 255) * brightness;
+        patternBrightness = brightness * patterns[pattern].brightness / 255;
     }
 
     DUTY_TYPE gammaValue = PGM_READ_DUTY(&gammaMap[(uint8_t)patternBrightness]);
